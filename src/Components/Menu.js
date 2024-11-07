797c46fe7fa30fcd54c237e911d983abf900ce1f
teak910 <hopeend1985@gmail.com>
2024-11-07 09:32:09 +0400

    Modifying components and ensuring working and testing

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import recipes from './recipes';
-// import Swal from 'sweetalert2'
+import Swal from 'sweetalert2'
 const Menu = () => {
 
   const handleOrder = (id) => {
     console.log(id, "id is clicked");
-    // Swal.fire({
-    //   title: "Are you sure?",
-    //   text: "You won't be able to revert this!",
-    //   icon: "warning",
-    //   showCancelButton: true,
-    //   confirmButtonColor: "#3085d6",
-    //   cancelButtonColor: "#d33",
-    //   confirmButtonText: "Yes, delete it!"
-    // }).then((result) => {
-    //   if (result.isConfirmed) {
-    //     Swal.fire({
-    //       title: "Deleted!",
-    //       text: "Your file has been deleted.",
-    //       icon: "success"
-    //     });
-    //   }
-    // });
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, order it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Ordered!",
+          text: "Your order has been getting ready.",
+          icon: "success"
+        });
+      }
+    });
   }
   return (
     <div className='menu-container'>
@@ -33,7 +33,7 @@ const Menu = () => {
       <div className='cards'>
         {
           recipes.map( recipe => <div key={recipe.id} className='menu-items'>
-            <img src={recipe.image} alt="" />
+            <img src={recipe.image} alt="why?" />
             <div className='menu-content'>
               <div className='heading'>
                 <h3>{recipe.title}</h3>
--- src/Components/recipes.js
+++ src/Components/recipes.js
@@ -4,15 +4,14 @@ const recipes = [
     id: 1,
     title: "Greek salad",
     price: 12.99,
-    image: "",
-    description:
-      "The famous greek salad of crispy lettuce, peppers, olives and our Chicago style fota cheese, garnished with crunchy garlic and rosemary croutons",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuKPX8mMNZ7K5inOJ5OkoLhsMQ5aPyju2x4Q&s",
+    description: "The famous greek salad of crispy lettuce, peppers, olives and our Chicago style fota cheese, garnished with crunchy garlic and rosemary croutons",
   },
   {
     id: 2,
     title: "Bruchetta",
     price: 5.99,
-    image: "",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRS6jiJGguQKGjUii74jOOEm6_hfttXWOEc9Q&s",
     description:
       "Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.",
   },
@@ -20,10 +19,12 @@ const recipes = [
     id: 3,
     title: "Lemon Dessert",
     price: 4.78,
-    image: "",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDW1CAQv734xKOmPutStiPxI16qXMf7dqDrQ&s",
     description:
       "This comes straight from grandma's recipe book, every last ingredient has been sourced and is as authentic as can be imagined.",
   },
 
 ];
+
 export default recipes;
+
